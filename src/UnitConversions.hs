a9515270e81bebd69f992b0db52f4690b19ca676
Nathaniel <nathanielps@gmail.com>
2017-10-11 12:28:52 -0500

    Added UnitConversions and skeleton funcitons

---
+++ src/UnitConversions.hs
@@ -0,0 +1,38 @@
+module UnitConversions where
+
+import Data.List
+import Data.Ratio
+
+data Unit = Unit Metric | Imperial
+
+convertRecipeUnits :: Unit -> Recipe -> Recipe
+convertRecipeUnits un recp =
+    if un = Metric then
+        recp{ingredients = map convertIngredientToMetric (ingredients recp)}
+    else
+        recp{ingredients = map convertIngredientToImperial (ingredients recp)}
+
+convertIngredientToMetric :: Ingredient -> Ingredient
+convertIngredientToMetric ingr =
+    case un of
+        "tsp"   -> ingr{quantity = qty * 5, unit = "mL"}
+        "Tbsp"  -> ingr{quantity = qty * 15, unit = "mL"}
+        "cup"   -> ingr{quantity = qty * 250, unit = "mL"}
+        "oz"    -> ingr{quantity = qty * 28, unit = "g"}
+    where
+        un = unit ingr,
+        qty = quantity ingr
+
+convertIngredientToImperial :: Ingredient -> Ingredient
+convertIngredientToImperial ingr =
+    case un of
+        "mL"   -> if qty < 15 then
+                      ingr{quantity = qty / 5, unit = "tsp"}
+                  else if qty < 250 then
+                      ingr{quantity = qty / 15, unit = "Tbsp"}
+                  else
+                      ingr{quantity = qty / 250, unit = "cup"}
+        "g"    -> ingr{quantity = qty / 28, unit = "oz"}
+    where
+        un = unit ingr,
+        qty = quantity ingr
