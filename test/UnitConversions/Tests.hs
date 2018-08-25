399049f37b50620cc051920f8547c5761a15210c
Langston Barrett <langston.barrett@gmail.com>
2018-08-25 14:18:10 -0700

    Use new Instances module in UnitConversions.Test

--- test/UnitConversions/Tests.hs
+++ test/UnitConversions/Tests.hs
@@ -1,23 +1,12 @@
 module UnitConversions.Tests where
 
 import           Test.Tasty                (TestTree, testGroup)
--- import           Test.Tasty.HUnit          (testCase)
 import           Test.Tasty.QuickCheck     (testProperty)
--- import           Test.HUnit                ((@?=))
 import           Test.QuickCheck           ((==>), (===))
 
--- New Arbitrary instances
-import           Generic.Random
-import           Test.QuickCheck.Arbitrary (Arbitrary(..))
-
 import           Types
+import           Instances()
 import           UnitConversions
-
-instance Arbitrary Unit where
-  arbitrary = genericArbitrary uniform
-
-instance Arbitrary Ingredient where
-  arbitrary = genericArbitrary uniform
 
 isMetric :: Unit -> Bool
 isMetric unit =
