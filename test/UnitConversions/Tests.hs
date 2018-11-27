93be7792962ff58715ad6b8e5e955756398c135e
Moritz Rumpf <moritz.rumpf@gmail.com>
2018-11-27 16:48:54 +0100

    Fix typo in comments

--- test/UnitConversions/Tests.hs
+++ test/UnitConversions/Tests.hs
@@ -34,7 +34,7 @@ temperatureUnitTests = testGroup "TemperatureConversions"
         (convertTemperatureToMetric "preheat to 110°F") @?= "preheat to 43°C"
     , testCase "Converting °C to °F in a text" $
         (convertTemperatureToImperial "preheat to 43°C") @?= "preheat to 109°F"
-        -- because of rounding we get 110° when converting F -> C -> F
+        -- because of rounding we get 109°F when converting 110° F -> °C -> °F
 
     , testCase "Converting a negative temperature" $
         (convertTemperatureToMetric "-20°F") @?= "-29°C"
