d8b37b66ee1f0ec9f6b350e3e93dfdb2444d36ba
Moritz Rumpf <moritz.rumpf@gmail.com>
2018-12-08 14:53:37 +0100

    Create a testCase for each tested temperature tests
    This helps with listing all cases and also lists multiple failures as opposed to the previous implementation.
    Bundled all tests in a new test function to avoid too many imports in test/Main.hs

--- test/UnitConversions/Tests.hs
+++ test/UnitConversions/Tests.hs
@@ -4,7 +4,7 @@ import           Test.Tasty                (TestTree, testGroup)
 import           Test.Tasty.QuickCheck     (testProperty)
 import           Test.Tasty.HUnit          (testCase)
 import           Test.QuickCheck           ((==>), (===))
-import           Test.HUnit                ((@?=), (@=?))
+import           Test.HUnit                ((@?=))
 import           Data.Tuple                (swap)
 
 import           Types
@@ -19,8 +19,14 @@ isMetric unit =
     G  -> True
     _  -> False
 
-tests :: TestTree
-tests = testGroup "IngredientConversions"
+test :: TestTree
+test = testGroup "ConversionTests"
+    [ ingredientTests
+    , temperatureUnitTests
+    , findingTemperaturesUnitTests ]
+
+ingredientTests :: TestTree
+ingredientTests = testGroup "IngredientConversions"
     [ testProperty "testToMetric" $
         \x -> isMetric (unit x) ==>
                 convertIngredientToMetric x === x
@@ -36,23 +42,24 @@ temperatureUnitTests = testGroup "TemperatureConversions"
     , testCase "Converting °C to °F in a text" $
         -- because of rounding we get 109°F when converting 110° F -> °C -> °F
         (convertTemperatureToImperial "preheat to 43°C") @?= "preheat to 109°F"
-        
-    , testCase "Recognizing temperatures in a text" $
-        let getTemperatures = map snd . findReplacements
-            testCases = [ ("250°C", [Temperature 250 C])
-                        , ("10 °C", [Temperature 10 C])
-                        , ("-10 °C", [Temperature (-10) C])
-                        , ("abc 15 C def", [Temperature 15 C])
-                        , ("5 C", [Temperature 5 C])
-                        , ("250C", [Temperature 250 C])
-                        , ("999°F", [Temperature 999 F])
-                        , ("34F", [Temperature 34 F])
-                        , ("250°C.", [Temperature 250 C])
-                        , ("250°C!", [Temperature 250 C])
-                        , ("250°C?", [Temperature 250 C])
-                        , ("10C 15F", [Temperature 10 C, Temperature 15 F])
-                        , ("0 Frites", [])
-                        , ("250Crabs", [])
-                        , ("250 Crabs", [])] :: [(String, [Temperature])] in
-            sequence_ $ map (uncurry (@=?) . fmap getTemperatures . swap) testCases
     ]
+
+findingTemperaturesUnitTests :: TestTree
+findingTemperaturesUnitTests = testGroup "recognizing/ignoring temperatures in a test" $
+    let getTemperatures = map snd . findReplacements
+        cases = [ ("250°C", [Temperature 250 C])
+                , ("10 °C", [Temperature 10 C])
+                , ("-10 °C", [Temperature (-10) C])
+                , ("abc 15 C def", [Temperature 15 C])
+                , ("5 C", [Temperature 5 C])
+                , ("250C", [Temperature 250 C])
+                , ("999°F", [Temperature 999 F])
+                , ("34F", [Temperature 34 F])
+                , ("250°C.", [Temperature 250 C])
+                , ("250°C!", [Temperature 250 C])
+                , ("250°C?", [Temperature 250 C])
+                , ("10C 15F", [Temperature 10 C, Temperature 15 F])
+                , ("0 Frites", [])
+                , ("250Crabs", [])
+                , ("250 Crabs", [])] :: [(String, [Temperature])] in
+        map ((\(exp, test) -> testCase test (getTemperatures test @?= exp)) . swap) cases
